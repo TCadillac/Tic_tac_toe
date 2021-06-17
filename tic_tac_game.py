856833723bba51892ad216a3000dbb510c3bfcb4
TCadillac <chauminh.triet1702@gmail.com>
2021-06-17 18:34:00 +0700

    Add human() and wincheck()

--- tic_tac_game.py
+++ tic_tac_game.py
@@ -8,12 +8,23 @@ play_option = input("Play with bot or human? ")
 
 while play_option.lower() != "human" and play_option.lower() != "bot":     
      play_option = input("Not a viable options, please type 'human' or 'bot': ") 
-
-
-play_option = input("Play with bot or human? ")  
+  
+def human():
+    print(table)
+    p1_choice = int(input("\nPlease enter a number from 1-9 to fill the table: "))
+    p2_choice = int(input("\nPlease enter a number from 1-9 to fill the table: "))
 
 if play_option == "human":     
-    pass 
+    human()
+else:
+    bot()
 
 def win_check():
-    pass
+    # Combinations that would lead to a win
+    win_list = [
+        [0,1,2], [3,4,5],
+        [6,7,8], [0,3,6],
+        [1,4,7], [2,5,8],
+        [0,4,8], [6,4,2],
+    ]
+    if table
