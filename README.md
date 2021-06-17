e1a435f9cd898dac36f80f96984f5501ba447eb6
TCadillac <chauminh.triet1702@gmail.com>
2021-06-17 18:19:54 +0700

    Add pass and improve readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# tic_tac_game
-A simple tic tac toe game
+# Tic Tac Toe
+A simple tic tac toe game with 2 play options:
+    - Player vs Player
+    - Player vs Bot
---
+++ tic_tac_game.py
@@ -0,0 +1,18 @@
+table = {    
+    " ", " ", " ",     
+    " ", " ", " ",    
+    " ", " ", " ", 
+    }  
+
+play_option = input("Play with bot or human? ") 
+
+while play_option.lower():     
+    pass 
+
+play_option = input("Play with bot or human? ")  
+
+if play_option == "human":     
+    pass 
+
+def win_check():
+    pass
