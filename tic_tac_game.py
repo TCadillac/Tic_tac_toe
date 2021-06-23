70a141715901305041af6b62651b46ef98538f15
TCadillac <chauminh.triet1702@gmail.com>
2021-06-23 15:05:33 +0700

    Remove global var, for retry()

--- tic_tac_game.py
+++ tic_tac_game.py
@@ -1,56 +1,44 @@
 import random
-import sys
-
-turn = "X"
-table = [    
-        "-", "-", "-",     
-        "-", "-", "-",    
-        "-", "-", "-", 
-        ]       
-
+import sys    
 
 def human():
     """
     Human play option, execute tic tac toe for 2 players
     function called when user choose 2 players option
     """
+    still_game = True
     display_board()
     # while table still have available space, run until all boxes are filled
-    while turn:
-        p1_choice = table_check()
-        table[p1_choice] = "X"
-        display_board()
-        p2_choice = table_check()
-        table[p2_choice] = "O"
-        display_board()
+    while still_game:
+        still_game = table_check("X")
+        still_game = table_check("O")
+    print("DRAW!")
     retry()
 
 
-def table_check() -> int:
+def table_check(move) -> int:
     """
     Check tic tac toe turn, make sure no duplicate or out of range input
     """
+    turn = [0,1,2,3,4,5,6,7,8]
 
     # - 1 to fit it to the real index of 0-8
     index = int(input("\nPlease enter a number from 1-9 to fill the table: ")) - 1
 
-    if turn == False:
-        return None
-    else:
-        while index not in turn:
-            display_board()
-            print("Not available or out of range, please try again: ")
-            index = int(input("Enter a number from 1-9: ")) - 1
-    
-        turn.remove(index)
-        return index
+    # If there's no winner and the table has been filled:
+    if len(turn) == 0:
+        return False
 
-def current_turn():
-    global turn
-    if turn == "X":
-        turn = "O"
-    else:
-        turn = "X"
+    # If user enter invalid number or numbers that are already filled:
+    while index not in turn:
+        display_board()
+        print("Not available or out of range, please try again: ")
+        index = int(input("Enter a number from 1-9: ")) - 1
+
+    table[index] = move
+    display_board(index, move)
+    turn.remove(index)
+    return True
 
         
 def win_check() -> bool:
@@ -74,12 +62,17 @@ def retry():
         print("You didnt enter 'y' or 'yes', so the game will end. Thanks for playing!")
 
 
-def display_board():
-  print("\n")
-  print(table[0] + " | " + table[1] + " | " + table[2] + "     1 | 2 | 3")
-  print(table[3] + " | " + table[4] + " | " + table[5] + "     4 | 5 | 6")
-  print(table[6] + " | " + table[7] + " | " + table[8] + "     7 | 8 | 9")
-  print("\n")
+def display_board(index):
+    table = [    
+        "-", "-", "-",     
+        "-", "-", "-",    
+        "-", "-", "-", 
+        ]  
+    print("\n")
+    print(table[0] + " | " + table[1] + " | " + table[2] + "     1 | 2 | 3")
+    print(table[3] + " | " + table[4] + " | " + table[5] + "     4 | 5 | 6")
+    print(table[6] + " | " + table[7] + " | " + table[8] + "     7 | 8 | 9")
+    print("\n")
 
 
 def game(play_option = "Blank"):
