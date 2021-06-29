c1e01e72d6607badc24cbb714a45394d0f4e2b98
TCadillac <chauminh.triet1702@gmail.com>
2021-06-29 14:46:49 +0700

    Add comments and clean code

--- tic_tac_game.py
+++ tic_tac_game.py
@@ -13,16 +13,15 @@ def human():
         "-", "-", "-", 
         ]
     choices = choice()
-    # Keep track of remaining moves, if empty, game is done
     turn = [0,1,2,3,4,5,6,7,8]
 
     # while table still have available space, run until all boxes are filled
     while len(turn) != 0:
         
         # Player1's turn
-        move_index, turn = table_check(table, turn)
-        table[move_index] = choices[0]
-        display_board(table)
+        move_index, turn = table_check(table, turn)   # Check if the index is valid
+        table[move_index] = choices[0]                # Fill X or O to the table base on the index chosen
+        display_board(table)                          # Display to let them see for 2nd player's turn
 
         # The game cannot be won unless 5 moves has been played, so when turn has been reduced to 4 moves or less, check win
         # Check win before tie since last move might make it a win
@@ -32,15 +31,15 @@ def human():
                 print(f"\nPlayer \"{player}\" won!!\nThanks for playing!")
                 retry()
 
-        # "X" will be the one who finish the game, so after filling the X into the table
+        # Player 1 will be the one who finish the game, so after filling every turn of player 1
         # we need to check if it's the last turn, if yes than break
         if len(turn) == 0:
             break
         
         # Player2's turn
-        move_index, turn = table_check(table, turn)
-        table[move_index] = choices[1]
-        display_board(table)
+        move_index, turn = table_check(table, turn)   # Check if the index is valid
+        table[move_index] = choices[1]                # Fill X or O to the table base on the index chosen
+        display_board(table)                          # Display to let them see for 2nd player's turn
 
         # The game cannot be won unless 5 moves has been played, so when turn has been reduced to 4 moves or less, check win
         if len(turn) <= 4:
@@ -69,9 +68,9 @@ def bot():
     while len(turn) != 0:
         
         # Player1 turn
-        move_index, turn = table_check(table, turn)
-        table[move_index] = choices[0]
-        display_board(table)
+        move_index, turn = table_check(table, turn)   # Check if the index is valid
+        table[move_index] = choices[0]                # Fill X or O to the table base on the index chosen
+        display_board(table)                          # Display to let them see for 2nd player's turn
 
         # The game cannot be won unless 5 moves has been played, so when turn has been reduced to 4 moves or less, check win
         # Check win before tie since last move might make it a win
@@ -87,11 +86,11 @@ def bot():
             break
         
         # Bot's turn
-        move_index = random.choice(turn)
-        turn.remove(move_index)
-        table[move_index] = choices[1]
+        move_index = random.choice(turn)  # Bot moves can just be chosen randomly from the
+        turn.remove(move_index)           # available moves from turn, so doesnt need to table_check()
+        table[move_index] = choices[1]    # Fill X or O to the table base on the index chosen
         print("Bot is thinking....")
-        time.sleep(random.randint(1,2))
+        time.sleep(random.randint(1,2))   # Make it realistic
 
         # The game cannot be won unless 5 moves has been played, so when turn has been reduced to 4 moves or less, check win
         if len(turn) <= 4:
@@ -154,7 +153,7 @@ def win_check(table: list) -> (bool, str):
         # Check rows, columns, and diagonals
         combination = set([table[line[0]], table[line[1]], table[line[2]]])
 
-        if len(combination) == 1 and combination != {"-"}:
+        if len(combination) == 1 and combination != {"-"}: # Which mean we have a straight line of either X or O
             #unpack comb (which is 1 item), which is either "X" or "O" to know who won
             return True, *combination
     else:
@@ -162,7 +161,6 @@ def win_check(table: list) -> (bool, str):
 
 
 def retry():
-
     for attempt in range(3):
         answer = input("Do you want to play again? y/n: ")
 
@@ -206,7 +204,7 @@ def display_board(table: list):
 
 
 def game():
-    play_option = "Blank"
+    play_option = ""
     while play_option.lower() != "human" and play_option.lower() != "bot":     
         play_option = input("Please type 'human' or 'bot': ") 
     
